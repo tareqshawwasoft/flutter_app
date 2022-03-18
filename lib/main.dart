bce54ac97bb1a5a96e69342934d7f4ff76c9186a
tareqshawwasoft <tareqsoftwork@gmail.com>
2022-03-18 23:14:44 +0200

    APPBAR FINAL commit

--- lib/main.dart
+++ lib/main.dart
@@ -66,12 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -79,7 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(
           widget.title,
           style: const TextStyle(color: Colors.black),
-        ), //Image.asset('assets/images/pic.png'
+        ),
         leading: Padding(
           padding: const EdgeInsets.all(8.0),
           child: ClipRRect(
